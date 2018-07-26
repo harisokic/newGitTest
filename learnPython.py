4152cfc42eebfddf5b429415d0af56edcf014196
Haris Okic <haris.okic7@gmail.com>
2018-07-26 10:28:57 -0500

    Someone else added a new line of code to this file

--- learnPython.py
+++ learnPython.py
@@ -1,3 +1,5 @@
 print("This is the first line of code")
 
 print("This is a new line of code that I added to the 'learnPython.py' file")
+
+print("Let's pretend that someone else added this line of code")
